ba25fc38661fd6a62796ed94ad601b914348fdde
ticn <61242872+ticn@users.noreply.github.com>
2021-02-04 14:01:51 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install rclone static binary
-wget -q https://github.com/xinxin8816/heroku-aria2c-21vianet/raw/master/rclone.zip
+wget -q https://raw.githubusercontent.com/ticn/heroku-ariang-21vianet/master/rclone.zip
 unzip -q rclone.zip
 export PATH=$PWD:$PATH
 chmod 777 /app/rclone
